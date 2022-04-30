f80dd65fe4021d78d48b9625d8afc4cb57bb7546
wade50110 <a0917940302@gmail.com>
2022-04-30 14:28:19 +0800

    修正幣別轉換中文功能

--- src/main/java/com/example/demo/util/CurrencyUtil.java
+++ src/main/java/com/example/demo/util/CurrencyUtil.java
@@ -1,14 +1,22 @@
 package com.example.demo.util;
 
 import java.util.Currency;
+import java.util.Locale;
 import java.util.Set;
 
 public class CurrencyUtil {
+	
+	/**
+	 * 給貨幣代號 回傳中文代稱
+	 * Ex : TWD   ->  新臺幣
+	 * @param numericCode
+	 * @return
+	 */
 	public static String getCurrencyInstance(String numericCode) {
 	    Set<Currency> currencies = Currency.getAvailableCurrencies();
 	    for (Currency currency : currencies) {
 	        if (currency.toString().equals(numericCode)) {
-	            return currency.toString();
+	            return currency.getDisplayName(Locale.TRADITIONAL_CHINESE);
 	        }
 	    }
 	    throw new IllegalArgumentException("Currency with numeric code "  + numericCode + " not found");
